0022e8bda70af4aad11da27fde9251712051c2b2
Manvel Saroyan <saroyanm@gmail.com>
2022-01-15 18:11:00 +0100

    web requests api

--- js/background.js
+++ js/background.js
@@ -1,1 +1,12 @@
-console.log("BACKGROUND PAGE LAUNCHED");
+const onSendHeaders = chrome.webRequest.onSendHeaders;
+const onHeadersReceived = chrome.webRequest.onHeadersReceived;
+
+
+const urls = {urls: ["<all_urls>"]};
+onSendHeaders.addListener((request) => {
+  console.log("onSendHeaders", request);
+}, urls, ["requestHeaders"]);
+
+onHeadersReceived.addListener((response) => {
+  console.log("onHeadersReceived", response);
+}, urls, ["responseHeaders"]);
