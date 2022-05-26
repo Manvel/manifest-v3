c9747c5797b31a104b699a03cb53f8968db30b2c
Manvel Saroyan <saroyanm@gmail.com>
2022-05-26 17:18:21 +0200

    Add button to page and create RPC connection with SW

--- js/background.js
+++ js/background.js
@@ -1,1 +1,10 @@
-console.log("BACKGROUND PAGE LAUNCHED");
+console.log("SERVICE WORKER");
+chrome.runtime.onConnect.addListener((port) => {
+  console.log("RPC CONNECTED");
+  port.onMessage.addListener((message) => {
+    console.log("REQUEST", message);
+    if (port.name = "RPC") {
+      port.postMessage({message: "answer"});
+    }
+  });
+});
--- js/cs/script.js
+++ js/cs/script.js
@@ -1,1 +1,9 @@
-console.log("CONTENT SCRIPT LAUNCHED");
+console.log("CONTENT SCRIPT")
+const port = chrome.runtime.connect({name: "RPC"});
+const button = document.createElement("button");
+button.textContent = "Send MSG to BG";
+button.addEventListener("click", () => {
+  port.postMessage({message: "Request"});
+});
+port.onMessage.addListener((resp) => console.log("RESPONSE", resp));
+document.querySelector("body").appendChild(button);
