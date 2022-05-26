be689e04b590f63a388eabafbf199a7c5abe06f9
Manvel Saroyan <saroyanm@gmail.com>
2022-05-26 18:30:39 +0200

    Keep service workers alive from the Content Script

--- js/cs/script.js
+++ js/cs/script.js
@@ -5,5 +5,10 @@ button.textContent = "Send MSG to BG";
 button.addEventListener("click", () => {
   port.postMessage({message: "Request"});
 });
-port.onMessage.addListener((resp) => console.log("RESPONSE", resp));
+port.onMessage.addListener((resp) => {
+  console.log("RESPONSE", resp);
+  setTimeout(() => {
+    port.postMessage({message: "Request"})
+  }, 30000);
+});
 document.querySelector("body").appendChild(button);
