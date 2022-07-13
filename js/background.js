abc8882dd117aab6dab2b285bf7ff8e1fb0dc2aa
Manvel Saroyan <saroyanm@gmail.com>
2022-07-13 11:54:06 +0200

    retrieve image

--- js/background.js
+++ js/background.js
@@ -1,1 +1,15 @@
 console.log("BACKGROUND PAGE LAUNCHED");
+
+chrome.runtime.onConnect.addListener((port) => {
+  console.log("RPC CONNECTED");
+  port.onMessage.addListener(async (message, sender) => {
+    console.log("REQUEST", sender);
+    const image = await chrome.tabs.captureVisibleTab();
+    console.log(image);
+
+    if (port.name = "RPC") {
+      // TODO: send image reference to the front-end and render it there.
+      port.postMessage({message: "answer"});
+    }
+  });
+});
--- js/cs/script.js
+++ js/cs/script.js
@@ -1,1 +1,14 @@
 console.log("CONTENT SCRIPT LAUNCHED");
+
+const port = chrome.runtime.connect({name: "RPC"});
+const button = document.createElement("button");
+button.textContent = "Send MSG to BG";
+button.addEventListener("click", () => {
+  port.postMessage({message: "Request"});
+});
+
+port.onMessage.addListener((resp) => {
+  console.log("RESPONSE", resp);
+});
+
+document.querySelector("body").appendChild(button);
